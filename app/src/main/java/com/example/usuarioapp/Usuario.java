381d91f42b1ced94a83992f25ef6559a59891e29
Dennisse Romero <dennisseromero@uees.edu.ec>
2025-07-03 23:12:01 -0500

    Primer commit

---
+++ app/src/main/java/com/example/usuarioapp/Usuario.java
@@ -0,0 +1,34 @@
+package com.example.usuarioapp;
+
+public class Usuario {
+
+    int id;
+    String nombre;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
